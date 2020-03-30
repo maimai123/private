44f2c8dfba6fda7a32aef4578ccdc626aaa85c03
maimai123 <yuyafei@linctex.com>
2020-03-30 20:34:28 +0800

    fix: 详情必填

--- src/views/demand/create/index.vue
+++ src/views/demand/create/index.vue
@@ -15,41 +15,42 @@
         <el-row>
           <el-col :span="12">
             <el-form-item label="头像" prop="avatar">
-              <UploadImg :url="form.avatar" field="avatar" @onSuccess="handleChoose" />
+              <UploadImg v-if="!isEdit" :url="form.avatar" field="avatar" @onSuccess="handleChoose" />
+              <img class="avatar" v-else :src="form.avatar" alt="avatar">
             </el-form-item>
           </el-col>
           <el-col :span="12">
             <el-form-item label="昵称" prop="name">
-              <el-input v-model="form.name" />
+              <el-input :disabled="isEdit" v-model="form.name" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row>
           <el-col :span="12">
             <el-form-item label="公司" prop="company">
-              <el-input v-model="form.company" />
+              <el-input :disabled="isEdit" v-model="form.company" />
             </el-form-item>
           </el-col>
           <el-col :span="12">
             <el-form-item label="职位" prop="job">
-              <el-input v-model="form.job" />
+              <el-input :disabled="isEdit" v-model="form.job" />
             </el-form-item>
           </el-col>
         </el-row>
         <div class="title">对接信息<span>（信息描述得越详细越高效）</span></div>
         <el-form-item label="我的需求" prop="needs">
-          <el-input type="textarea" v-model="form.needs" placeholder="示例：1.小程序开发 2.网站开发"/>
+          <el-input type="textarea" :disabled="isEdit" v-model="form.needs" placeholder="示例：1.小程序开发 2.网站开发"/>
         </el-form-item>
         <el-form-item label="需求标签选择" prop="needs_tag" class="item-content">
-          <el-select v-model="form.needs_tag" multiple placeholder="请选择">
+          <el-select :disabled="isEdit" v-model="form.needs_tag" multiple placeholder="请选择">
             <el-option v-for="item in needs" :key="item.id" :label="item.title" :value="item.id" />
           </el-select>
         </el-form-item>
         <el-form-item label="我的资源" prop="resource">
-          <el-input type="textarea" v-model="form.resource" placeholder="示例：1.100万粉丝群 2.日活1000+产品"/>
+          <el-input :disabled="isEdit" type="textarea" v-model="form.resource" placeholder="示例：1.100万粉丝群 2.日活1000+产品"/>
         </el-form-item>
         <el-form-item label="资源标签选择" prop="resource_tag" class="item-content">
-          <el-select v-model="form.resource_tag" multiple placeholder="请选择">
+          <el-select :disabled="isEdit" v-model="form.resource_tag" multiple placeholder="请选择">
             <el-option v-for="item in resource" :key="item.id" :label="item.title" :value="item.id" />
           </el-select>
         </el-form-item>
@@ -62,32 +63,35 @@
                 v-else
                 :options="location"
                 :props="format"
+                :disabled="isEdit"
                 :separator="'-'"
                 v-model="form.city"
               ></el-cascader>
             </el-form-item>
             <el-form-item label="" prop="address" class="item-content">
-              <el-input v-model="form.address" placeholder="请输入所在详细地址" style="width: 300px;" />
+              <el-input :disabled="isEdit" v-model="form.address" placeholder="请输入所在详细地址" style="width: 300px;" />
             </el-form-item>
           </div>
         </el-form-item>
         <el-form-item label="联系方式" prop="contact">
-          <el-input v-model="form.contact" />
+          <el-input :disabled="isEdit" v-model="form.contact" />
         </el-form-item>
         <el-form-item label="类型" prop="type" class="item-content">
-          <el-radio v-model="form.type" :label="1">个人</el-radio>
-          <el-radio v-model="form.type" :label="2">公司</el-radio>
+          <el-radio :disabled="isEdit" v-model="form.type" :label="1">个人</el-radio>
+          <el-radio :disabled="isEdit" v-model="form.type" :label="2">公司</el-radio>
         </el-form-item>
         <el-form-item label="" prop="photo">
-          <UploadImg :url="form.photo" field="photo" @onSuccess="handleChoose" />
-          <div class="item-content tip">{{ form.type === 1 ? '请上传个人工牌照' : '请上传公司营业执照' }}</div>
+          <UploadImg v-if="!isEdit" :url="form.photo" field="photo" @onSuccess="handleChoose" />
+          <img class="photo" v-else :src="form.photo" alt="avatar">
+          <div v-if="!isEdit" class="item-content tip">{{ form.type === 1 ? '请上传个人工牌照' : '请上传公司营业执照' }}</div>
         </el-form-item>
         <el-form-item label="我的产品" prop="product_code">
-          <UploadImg :url="form.product_code" field="product_code" @onSuccess="handleChoose" />
-          <div class="item-content tip">请上传产品二维码</div>
+          <UploadImg v-if="!isEdit" :url="form.product_code" field="product_code" @onSuccess="handleChoose" />
+          <img class="photo" v-if="isEdit && form.product_code" :src="form.product_code" alt="avatar">
+          <div v-if="!isEdit" class="item-content tip">请上传产品二维码</div>
         </el-form-item>
         <el-form-item label="是否显示产品图" v-if="form.product_code" prop="is_show" class="item-content">
-          <el-switch v-model="form.is_show" :active-value="1" :inactive-value="0" />
+          <el-switch :disabled="isEdit" v-model="form.is_show" :active-value="1" :inactive-value="0" />
         </el-form-item>
         <el-form-item class="operate-btns">
           <template v-if="form.status === 0">
@@ -134,6 +138,9 @@ export default {
         else title = '查看详情';
       }
       return title;
+    },
+    isEdit () {
+      return this.id && this.id !== 'main';
     }
   },
 
